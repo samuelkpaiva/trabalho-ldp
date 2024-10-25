30ccf5c5405f1f1f40366b45f7882da315cd32d7
Kaiky Leite <kaiky.leite@estudante.ifgoiano.edu.br>
2024-10-25 01:32:00 -0300

    Atributos e método construtor

--- Biblioteca/src/main/java/biblioteca/Cliente.java
+++ Biblioteca/src/main/java/biblioteca/Cliente.java
@@ -2,5 +2,16 @@
 package biblioteca;
 
 public class Cliente {
-    
+
+    private String nome;
+    private String email;
+    private int id;
+
+    public Cliente(String nome, String email, int id) {
+        this.nome = nome;
+        this.email = email;
+        this.id = id;
+
+    }
+
 }
