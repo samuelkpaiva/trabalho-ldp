db87aac358edfcf64650df90328d790a79bccd4d
samuelkpaiva <samuelkpaiva@gmail.com>
2024-10-25 11:30:56 -0300

    esboco_app

---
+++ Biblioteca/src/main/java/biblioteca/App.java
@@ -0,0 +1,25 @@
+package biblioteca;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Selecione uma opção");
+        System.out.println("1 - Cadastrar Livro" + "\n" 
+                + "2 - Cadastrar Cliente" 
+                + "\n" + "3 - Realizar empréstimo ou devolução" 
+                + "\n" + "4 - Verificar clientes, livros ou empréstimos cadastrados");
+        int opcao = scanner.nextInt();
+        
+        switch(opcao){
+            case 1:{
+                System.out.println("Informe o título do livro");
+                System.out.println("Informe os autores do livro");
+                System.out.println("Informe o ano de publicação do livro");
+                
+            }  
+        }
+    }
+}
