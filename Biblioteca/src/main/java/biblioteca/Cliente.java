cbd8ea0962741bf7ccab23ef913608b3cdac1a6d
Kaiky Leite <kaiky.leite@estudante.ifgoiano.edu.br>
2024-10-25 01:37:54 -0300

    métodos getters e setters

--- Biblioteca/src/main/java/biblioteca/Cliente.java
+++ Biblioteca/src/main/java/biblioteca/Cliente.java
@@ -13,5 +13,28 @@ public class Cliente {
         this.id = id;
 
     }
-
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public int getID(){
+        return id;
+    }
+    
+    public void getID(int id){
+        this.id = id;
+    }
 }
