2ff26e786b049d7bea8b740103852ae5ad54dff6
samuelkpaiva <samuelkpaiva@gmail.com>
2024-10-25 20:06:56 -0300

    Update Cliente.java

--- Biblioteca/src/main/java/biblioteca/Cliente.java
+++ Biblioteca/src/main/java/biblioteca/Cliente.java
@@ -1,5 +1,43 @@
+<<<<<<< HEAD
+=======
+
+>>>>>>> cbd8ea0962741bf7ccab23ef913608b3cdac1a6d
 package biblioteca;
 
 public class Cliente {
+
+    private String nome;
+    private String email;
+    private int id;
+
+    public Cliente(String nome, String email, int id) {
+        this.nome = nome;
+        this.email = email;
+        this.id = id;
+
+    }
     
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public int getID(){
+        return id;
+    }
+    
+    public void getID(int id){
+        this.id = id;
+    }
 }
