7f61050f009d4c9041c5e9813cde0771c595a659
samuelkpaiva <samuelkpaiva@gmail.com>
2024-10-26 00:29:50 -0300

    Update App.java

--- Biblioteca/src/main/java/biblioteca/App.java
+++ Biblioteca/src/main/java/biblioteca/App.java
@@ -13,50 +13,52 @@ public class App {
                 + "\n" + "4 - Verificar clientes, livros ou empréstimos cadastrados");
         int opcao = scanner.nextInt();
         
-        switch(opcao){
-            case 1:{
-                System.out.println("Informe o título do livro");
-                System.out.println("Informe os autores do livro");
-                System.out.println("Informe o ano de publicação do livro");
-                
-                break;
-            }  
-            case 2: {
-                System.out.println("Informe o nome do cliente");
-                System.out.println("Informe o email do cliente");
-                
-                break;
-            }
-            case 3: {
-                System.out.println("Digite " + "\n" 
-                        + "1 para empréstimo" + "\n" 
-                        + "2 para devolução");
-                int emp_dev = scanner.nextInt();
-                
-                if(emp_dev == 1){
-                    System.out.println("Informe a data de empréstimo");
-                    System.out.println("Informe o código do livro");
-                } else {
-                    System.out.println("Informe o código do livro");
+        do{
+            switch(opcao){
+                case 1:{
+                    System.out.println("Informe o título do livro");
+                    System.out.println("Informe os autores do livro");
+                    System.out.println("Informe o ano de publicação do livro");
+
+                    break;
+                }  
+                case 2: {
+                    System.out.println("Informe o nome do cliente");
+                    System.out.println("Informe o email do cliente");
+
+                    break;
                 }
-                break;
-            }
-            case 4: {
-                System.out.println("Digite " + "\n" 
-                        + "1 para vizualizar clientes cadastrados" + "\n" 
-                        + "2 para vizualizar livros cadastrados" + "\n" 
-                        + "3 para vizualizar empréstimos cadastrados");
-                int opc_vizu = scanner.nextInt();
-                
-                if(opc_vizu == 1){
-                    
-                } else if(opc_vizu == 2){
-                    
-                } else {
-                    
+                case 3: {
+                    System.out.println("Digite " + "\n" 
+                            + "1 para empréstimo" + "\n" 
+                            + "2 para devolução");
+                    int emp_dev = scanner.nextInt();
+
+                    if(emp_dev == 1){
+                        System.out.println("Informe a data de empréstimo");
+                        System.out.println("Informe o nome do livro");
+                    } else {
+                        System.out.println("Informe o código do livro");
+                    }
+                    break;
                 }
-                break;
+                case 4: {
+                    System.out.println("Digite " + "\n" 
+                            + "1 para vizualizar clientes cadastrados" + "\n" 
+                            + "2 para vizualizar livros cadastrados" + "\n" 
+                            + "3 para vizualizar empréstimos cadastrados");
+                    int opc_vizu = scanner.nextInt();
+
+                    if(opc_vizu == 1){
+
+                    } else if(opc_vizu == 2){
+
+                    } else {
+
+                    }
+                    break;
+                }
             }
-        }
+        } while(opcao == 5);
     }
 }
