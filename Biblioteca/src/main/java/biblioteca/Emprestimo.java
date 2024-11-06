d61ca1cd601ba6cf3d874ced611e1f477f5e8fda
Leandro1737 <leandromacielhti2016@gmail.com>
2024-11-06 20:00:11 -0300

    atualização Emprestimo

--- Biblioteca/src/main/java/biblioteca/Emprestimo.java
+++ Biblioteca/src/main/java/biblioteca/Emprestimo.java
@@ -1,5 +1,27 @@
 package biblioteca;
 
 public class Emprestimo {
-    String data_emprestimo;
+    private Livros livro;
+    private String dataEmprestimo;
+
+    public Livros getLivro() {
+        return livro;
+    }
+    public Emprestimo(Livros livro, String dataEmprestimo) {
+        this.livro = livro;
+        this.dataEmprestimo = dataEmprestimo;
+        
+        // Realiza o empréstimo, caso tenha exemplares disponíveis
+        if (!livro.emprestarExemplar()) {
+            System.out.println("Empréstimo não realizado. Sem exemplares disponíveis.");
+        } else {
+            System.out.println("Empréstimo realizado com sucesso!");
+        }
+    }
+
+    public void devolver() {
+        livro.devolverExemplar();
+        System.out.println("Livro devolvido com sucesso!");
+    }
 }
+
