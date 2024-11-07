cda3049a33d761f2d1b739d3589b226711cc20ab
samuelkpaiva <samuelkpaiva@gmail.com>
2024-11-07 09:36:28 -0300

    conserto de bugs
    Arrumando os bugs de id do cliente, adicionando try catch, limpando o código

--- Biblioteca/src/main/java/biblioteca/App.java
+++ Biblioteca/src/main/java/biblioteca/App.java
@@ -1,5 +1,6 @@
 package biblioteca;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -10,8 +11,9 @@ public class App {
         Livros[] livros = new Livros[3];
         Emprestimo[] emprestimos = new Emprestimo[3];
         int contadorLivro = 0, contadorCliente = 0, contadorEmprestimo = 0;
+        int proximoID = 1;
 
-        int opcao;
+        int opcao = 0;
         do {
             System.out.println("Selecione uma opção");
             System.out.println("1 - Cadastrar Livro"
@@ -20,11 +22,25 @@ public class App {
                     + "\n" + "4 - Realizar Devolução"
                     + "\n" + "5 - Verificar clientes, livros ou empréstimos cadastrados"
                     + "\n" + "6 - Salvar e Sair");
-            opcao = scanner.nextInt();
-            scanner.nextLine(); // Limpar o buffer
+
+            boolean entrada = false;
+            while (!entrada) {
+                try {
+                    opcao = scanner.nextInt();
+                    scanner.nextLine(); 
+                    if (opcao < 1 || opcao > 6) {
+                        System.out.println("Por favor, digite uma opção válida entre 1 e 6.");
+                    } else {
+                        entrada = true;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Entrada inválida. Por favor, digite um número entre 1 e 6.");
+                    scanner.nextLine(); 
+                }
+            }
 
             switch (opcao) {
-                case 1: { // Cadastrar Livro
+                case 1: {
                     if (contadorLivro < livros.length) {
                         System.out.println("Informe o título do livro:");
                         String titulo = scanner.nextLine();
@@ -37,7 +53,7 @@ public class App {
 
                         System.out.println("Informe o número de exemplares do livro:");
                         int numExemplares = scanner.nextInt();
-                        scanner.nextLine(); // Limpar o buffer
+                        scanner.nextLine();
 
                         livros[contadorLivro] = new Livros(titulo, autores, anoPublicacao, numExemplares);
                         contadorLivro++;
@@ -47,16 +63,16 @@ public class App {
                         String resposta = scanner.nextLine();
                         if (resposta.equalsIgnoreCase("S")) {
                             Livros.listarLivros(livros);
-                            System.out.println("Digite o nome do livro que deseja remover:");
-                            String nomeParaRemover = scanner.nextLine();
-                            if (Livros.removerLivro(livros, nomeParaRemover)) {
+                            System.out.println("Digite o título do livro que deseja remover:");
+                            String tituloParaRemover = scanner.nextLine();
+                            if (Livros.removerLivro(livros, tituloParaRemover)) {
                                 contadorLivro--;
                             }
                         }
                     }
                     break;
                 }
-                case 2: { // Cadastrar Cliente
+                case 2: {
                     if (contadorCliente < clientes.length) {
                         System.out.println("Informe o nome do cliente:");
                         String nome = scanner.nextLine();
@@ -64,9 +80,9 @@ public class App {
                         System.out.println("Informe o email do cliente:");
                         String email = scanner.nextLine();
 
-                        int id = contadorCliente + 1;
-                        clientes[contadorCliente] = new Cliente(nome, email, id);
+                        clientes[contadorCliente] = new Cliente(nome, email, proximoID);
                         contadorCliente++;
+                        proximoID++;
                     } else {
                         System.out.println("Limite de clientes atingido.");
                         System.out.println("Deseja apagar algum cliente? S ou N");
@@ -75,22 +91,22 @@ public class App {
                             Cliente.listarClientes(clientes);
                             System.out.println("Digite o ID do cliente que deseja remover:");
                             int idParaRemover = scanner.nextInt();
-                            scanner.nextLine(); // Limpar o buffer
+                            scanner.nextLine();
                             if (Cliente.removerCliente(clientes, idParaRemover)) {
                                 contadorCliente--;
-                                System.out.println("Cliente removido com sucesso.");
+                                System.out.println("Cliente removido");
                             } else {
-                                System.out.println("ID inválido ou cliente não encontrado.");
+                                System.out.println("Não foi encontrado um cliente com esses dados.");
                             }
                         }
                     }
                     break;
                 }
-                case 3: { // Realizar Empréstimo
+                case 3: {
                     if (contadorEmprestimo < emprestimos.length) {
                         System.out.println("Informe o ID do cliente:");
                         int clienteId = scanner.nextInt();
-                        scanner.nextLine(); // Limpar o buffer
+                        scanner.nextLine();
 
                         if (clienteId > 0 && clienteId <= clientes.length && clientes[clienteId - 1] != null) {
                             Cliente cliente = clientes[clienteId - 1];
@@ -99,82 +115,75 @@ public class App {
                             String tituloLivro = scanner.nextLine();
 
                             Livros livro = null;
-                            // Procurar o livro pelo título
                             for (Livros l : livros) {
-                                if (l != null && l.gettitulo().equalsIgnoreCase(tituloLivro)) {
+                                if (l != null && l.titulo.equalsIgnoreCase(tituloLivro)) {
                                     livro = l;
                                     break;
                                 }
                             }
 
                             if (livro != null) {
-                                // Verificar se há exemplares disponíveis antes de criar o empréstimo
                                 if (livro.emprestarExemplar()) {
                                     emprestimos[contadorEmprestimo] = new Emprestimo(livro, "Data do Empréstimo");
                                     contadorEmprestimo++;
-                                    System.out.println("Empréstimo realizado com sucesso!");
+                                    System.out.println("Empréstimo realizado");
                                 } else {
                                     System.out.println("Não há exemplares disponíveis para empréstimo.");
                                 }
                             } else {
-                                System.out.println("Livro com o título informado não encontrado.");
+                                System.out.println("Livro não encontrado.");
                             }
                         } else {
-                            System.out.println("ID de cliente inválido.");
+                            System.out.println("ID de cliente não encontrado.");
                         }
                     } else {
                         System.out.println("Limite de empréstimos atingido.");
                     }
                     break;
                 }
-                case 4: { // Realizar Devolução
+                case 4: {
                     System.out.println("Informe o título do livro para devolução:");
                     String tituloLivro = scanner.nextLine();
 
                     Livros livro = null;
-                    // Procurar o livro pelo título
                     for (Livros l : livros) {
-                        if (l != null && l.gettitulo().equalsIgnoreCase(tituloLivro)) {
+                        if (l != null && l.titulo.equalsIgnoreCase(tituloLivro)) {
                             livro = l;
                             break;
                         }
                     }
 
                     if (livro != null) {
-                        // Devolvendo o livro e aumentando o número de exemplares
-                        livro.devolverExemplar();  // Aumenta o número de exemplares
-                        System.out.println("Livro devolvido com sucesso!");
+                        livro.devolverExemplar();
+                        System.out.println("Livro devolvido");
 
-                        // Remover o empréstimo correspondente
                         boolean emprestimoRemovido = false;
                         for (int i = 0; i < emprestimos.length; i++) {
-                            if (emprestimos[i] != null && emprestimos[i].getLivro().gettitulo().equalsIgnoreCase(tituloLivro)) {
-                                emprestimos[i] = null; // Remover o empréstimo
+                            if (emprestimos[i] != null && emprestimos[i].getLivro().titulo.equalsIgnoreCase(tituloLivro)) {
+                                emprestimos[i] = null;
                                 emprestimoRemovido = true;
-                                contadorEmprestimo --;
+                                contadorEmprestimo--;
                                 break;
                             }
                         }
 
                         if (emprestimoRemovido) {
-                            System.out.println("Empréstimo removido com sucesso!");
+                            System.out.println("Empréstimo removido");
                         } else {
-                            System.out.println("Empréstimo não encontrado.");
+                            System.out.println("Empréstimo não encontrado");
                         }
-
                     } else {
-                        System.out.println("Livro com o título informado não encontrado.");
+                        System.out.println("Livro não encontrado.");
                     }
                     break;
                 }
-
-                case 5: { // Verificação
+                case 5: {
                     System.out.println("Digite " + "\n"
                             + "1 para visualizar clientes cadastrados" + "\n"
                             + "2 para visualizar livros cadastrados" + "\n"
                             + "3 para visualizar empréstimos cadastrados");
                     int opcVizu = scanner.nextInt();
-                    scanner.nextLine(); // Limpar o buffer
+                    scanner.nextLine();
 
                     if (opcVizu == 1) {
                         Cliente.listarClientes(clientes);
@@ -186,15 +195,12 @@ public class App {
                     break;
                 }
                 case 6: {
-                    // Salvar dados no arquivo e sair
                     Salvamentos.salvarDadosEmArquivo(clientes, livros, emprestimos);
-                    System.out.println("Saindo...");
                     break;
                 }
-                default:
-                    System.out.println("Opção inválida.");
             }
         } while (opcao != 6);
-        scanner.close(); // Fechar o scanner
+
+        scanner.close();
     }
 }
