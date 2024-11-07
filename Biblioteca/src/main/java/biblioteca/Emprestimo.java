913d72474c43f84ed875fa99e85ed401fc3348dd
Kaiky Leite <kaiky.leite@estudante.ifgoiano.edu.br>
2024-11-07 09:30:52 -0300

    Atualizaçao Emprestimo
    resolvendo bugs

--- Biblioteca/src/main/java/biblioteca/Emprestimo.java
+++ Biblioteca/src/main/java/biblioteca/Emprestimo.java
@@ -7,31 +7,33 @@ public class Emprestimo {
     public Livros getLivro() {
         return livro;
     }
-    
-    public String getDataEmprestimo(){
+
+    public String getDataEmprestimo() {
         return dataEmprestimo;
     }
-    
+
     public Emprestimo(Livros livro, String dataEmprestimo) {
         this.livro = livro;
         this.dataEmprestimo = dataEmprestimo;
     }
+
     public static void listarEmprestimos(Emprestimo[] emprestimos) {
-    System.out.println("Empréstimos realizados:");
-    boolean encontrouEmprestimos = false; // Para verificar se há empréstimos
-    for (Emprestimo emprestimo : emprestimos) {
-        if (emprestimo != null) {
-            System.out.println("Livro: " + emprestimo.getLivro().gettitulo() + " - Data do Empréstimo: " + emprestimo.getDataEmprestimo());
-            encontrouEmprestimos = true;
+        System.out.println("Empréstimos realizados:");
+        boolean encontrouEmprestimos = false;
+        for (Emprestimo emprestimo : emprestimos) {
+            if (emprestimo != null) {
+                System.out.println("Livro: " + emprestimo.getLivro().titulo + " - Data do Empréstimo: " + emprestimo.getDataEmprestimo());
+                encontrouEmprestimos = true;
+            }
+        }
+
+        if (!encontrouEmprestimos) {
+            System.out.println("Nenhum empréstimo realizado.");
         }
     }
 
-    if (!encontrouEmprestimos) {
-        System.out.println("Nenhum empréstimo realizado.");
-    }
-}
     public void devolver() {
         livro.devolverExemplar();
         System.out.println("Livro devolvido com sucesso!");
-    }
+    }
 }
