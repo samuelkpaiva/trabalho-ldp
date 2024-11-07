53722ab95e85752093e004180b5280b9189fae73
Kaiky Leite <kaiky.leite@estudante.ifgoiano.edu.br>
2024-11-07 09:25:32 -0300

    Atualização do salvamentos
    A classe salvamentos teve que ser modificada para que nao desse erro com   a classe livros que agora possui algumas variaveis publicas

--- Biblioteca/src/main/java/biblioteca/Salvamentos.java
+++ Biblioteca/src/main/java/biblioteca/Salvamentos.java
@@ -7,7 +7,6 @@ import java.io.PrintWriter;
 public class Salvamentos {
     public static void salvarDadosEmArquivo(Cliente[] clientes, Livros[] livros, Emprestimo[] emprestimos) {
         try (PrintWriter writer = new PrintWriter(new FileWriter("biblioteca.txt"))) {
-            // Escrever informações dos clientes
             writer.println("Clientes Cadastrados:");
             for (Cliente cliente : clientes) {
                 if (cliente != null) {
@@ -15,27 +14,25 @@ public class Salvamentos {
                 }
             }
 
-            // Escrever informações dos livros
-            writer.println();  // Linha em branco entre clientes e livros
+            writer.println();  
             writer.println("Livros Cadastrados:");
             for (Livros livro : livros) {
                 if (livro != null) {
-                    writer.println("Título: " + livro.gettitulo() + ", Autor: " + livro.getautores() + ", Ano: " + livro.getanoPublicacao() + ", Exemplares: " + livro.getnumExemplares());
+                    writer.println("Título: " + livro.titulo + ", Autor: " + livro.autores + ", Ano: " + livro.anoPublicacao + ", Exemplares: " + livro.getnumExemplares());
                 }
             }
 
-            // Escrever informações dos empréstimos
-            writer.println();  // Linha em branco entre livros e empréstimos
-            writer.println("Empréstimos Realizados:");
+            writer.println();  
+            writer.println("Empréstimos Ativos:");
             for (Emprestimo emprestimo : emprestimos) {
                 if (emprestimo != null) {
-                    writer.println("Livro: " + emprestimo.getLivro().gettitulo() + ", Data: " + emprestimo.getDataEmprestimo());
+                    writer.println("Livro: " + emprestimo.getLivro().titulo + ", Data: " + emprestimo.getDataEmprestimo());
                 }
             }
 
             System.out.println("Dados salvos no arquivo biblioteca.txt");
         } catch (IOException e) {
             System.out.println("Erro ao salvar dados em arquivo: " + e.getMessage());
-        }
-    }
+        }
+    }
 }
